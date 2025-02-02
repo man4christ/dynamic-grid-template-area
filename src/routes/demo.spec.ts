f927ff74810d82d739340b3ebb03723f31197be0
man4christ <man4christ.65qa8@simplelogin.com>
2025-02-02 17:30:18 -0500

    node left and right. renders correct with lc horizontal repeat.

--- src/routes/demo.spec.ts
+++ src/routes/demo.spec.ts
@@ -2,7 +2,8 @@ import { assert, describe, expect, it } from 'vitest'
 
 interface node {
   id: string;
-  next: node | any;
+  left: node | any;
+  right: node | any;
   split: string;
 }
 
@@ -11,41 +12,52 @@ interface rootNode {
   right: node;
 }
 
+function add2(l, r, split) {
 
-function add(n: node): any[] {
-  if (n.next === undefined) {
+  let ll = l.length
+  let rl = r.length
+
+  let lc = l[0].length
+  let rc = r[0].length
+
+
+
+  let com = [];
+  if (split === 'v') {
+    for (let i = 0; i < ll; i++) {
+      com.push(l[i].concat(r[i]));
+    }
+  }
+
+  if (split === 'h') {
+
+    let lrepeat = rc / lc;
+    for (let i = 0; i < ll; i++) {
+      let repeatRow = []
+      for (let j = 0; j < lc; j++) {
+        for (let k = 0; k < lrepeat; k++) {
+          repeatRow.push(l[i][j])
+        }
+        com.push(repeatRow)
+      }
+    }
+    for (let i = 0; i < rl; i++) {
+      com.push(r[i])
+    }
+  }
+
+  return com;
+}
+
+function add(n: node) {
+  if (n.split === undefined && n.id !== 'root') {
     return [[n.id]];
   } else {
-    let g = add(n.next);
-    let row = g.length;
-    let col = g[0].length;
-    let ng = [];
-
-    for (let i = 0; i < row; i++) {
-      let r = [];
-      for (let i = 0; i < col; i++) {
-        r.push(n.id);
-      }
-      ng.push(r);
-    }
-
-    let com = [];
-    if (n.split === 'v') {
-      for (let i = 0; i < row; i++) {
-        com.push(ng[i].concat(g[i]));
-      }
-    }
-
-    if (n.split === 'h') {
-      for (let i = 0; i < row; i++) {
-        com.push(ng[i])
-      }
-      for (let i = 0; i < row; i++) {
-        com.push(g[i])
-      }
-    }
-
-    return com;
+    let lg = add(n.left)
+    let rg = add(n.right)
+    let l = add2(lg, rg, n.split);
+    console.log(l)
+    return l
   }
 }
 
@@ -53,28 +65,34 @@ describe('suite name', () => {
   it('foo', () => {
     let root = {
       id: 'root',
+      split: 'h',
       left: {
-        id: '1',
         split: 'v',
-        next: {
-          id: '4',
+        left: {
+          id: '1',
         },
+        right: {
+          id: 4
+        },
+
       },
       right: {
-        id: '2',
         split: 'h',
-        next: {
-          id: '3',
+        left: {
+          id: '2'
+        },
+        right: {
           split: 'v',
-          next: {
+          left: {
+            id: '3',
+          },
+          right: {
             id: '5',
           },
         },
       },
     }
-
-    console.log(add(root.right))
-    console.log(add(root.left))
+    add(root)
   })
 
 })
